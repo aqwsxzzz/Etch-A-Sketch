fa69de100086f38440df13132d19c83c5e700e7c
ShegoS <jbarretolarrosa@gmail.com>
2021-07-29 16:14:26 -0300

    Added reset css file, created pad and start button

---
+++ script.js
@@ -0,0 +1,44 @@
+//Creates a button to start it all.
+let head = document.getElementById("head");
+let start = document.createElement("button");
+start.setAttribute("id", "button")
+start.textContent = "New Pad";
+head.appendChild(start);
+
+
+//Make an input to get the users info bout how much squares it
+//want per side.
+function askSquaresF() {
+  let askSquares = prompt("How much squres per side? Max = 100.");
+  if (askSquares > 100) {
+    alert("Max squares per side = 100");
+  } else createNewGrid(askSquares);
+  console.log(askSquares);
+}
+
+//Create the new grid based on user's squares quantity selection.
+//Also config the squares aspect.
+function createNewGrid(askSquares) {
+  for (let i = 0; i < askSquares * askSquares; i++) {
+    let container = document.getElementById("container");
+    let square = document.createElement("div");
+    container.style.setProperty("--askSquares", askSquares);
+    container.appendChild(square);
+    square.setAttribute("id", "square" + [i]);
+    square.setAttribute("class", "square");
+    square.addEventListener("mouseover", () =>
+      changeToPurpleColor("square" + [i])
+    );
+  }
+}
+
+//Change BG color of square to purple while mouse over it.
+function changeToPurpleColor(square) {
+  document.getElementById(square).style.backgroundColor = "purple";
+  document.getElementById(square).style.border = "0.1px solid black";
+}
+//Change back BG color to white after mouse leave the square.
+function changeToWhiteColor(square) {
+  document.getElementById(square).style.backgroundColor = "white";
+  document.getElementById(square).style.border = "0.1px solid black";
+}
